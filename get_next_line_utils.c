276468b0c37bc66ee84d14c19fc50639f3e7750b
Guilherme Fantoni Borges <gfantoni@student.42sp.org.br>
2023-08-23 13:23:45 -0300

    Marvin

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -5,117 +5,83 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/17 09:00:20 by gfantoni          #+#    #+#             */
-/*   Updated: 2023/08/17 09:08:07 by gfantoni         ###   ########.fr       */
+/*   Created: 2023/08/23 09:32:39 by gfantoni          #+#    #+#             */
+/*   Updated: 2023/08/23 11:26:56 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_new_pre_line(char	*pre_line)
+size_t	ft_strlen(char *str)
 {
-	char	*str;
-	int		i;
-	int		j;
+	size_t i;
 
 	i = 0;
-	while (pre_line[i] && pre_line[i] != '\n')
-		i++;
-	if (pre_line == NULL)
-	{
-		free(pre_line);
-		return (NULL);
-	}
-	str = malloc(sizeof(char) * (ft_strlen(pre_line) - i + 1));
-	if (str == NULL)
-		return (NULL);
-	i++;
-	j = 0;
-	while (pre_line[i])
-		str[j++] = pre_line[i++];
-	str[j] = '\0';
-	free(pre_line);
-	return (str);
-}
-
-char	*ft_get_line(char *pre_line)
-{
-	char	*str;
-	int		i;
-
-	i = 0;
-	while (pre_line[i] && pre_line[i] != '\n')
-		i++;
-	str = malloc(sizeof(char) * (i + 2));
-	if (str == NULL)
-		return (NULL);
-	i = 0;
-	while (pre_line[i] && pre_line[i] != '\n')
-	{
-		str[i] = pre_line[i];
-		i++;
-	}
-	if (pre_line[i] == '\n')
-	{
-		str[i] = pre_line[i];
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
-}
-
-int	ft_strlen(char *str)
-{
-	size_t	i;
-
-	if (str == NULL)
+	if (!str)
 		return (0);
-	i = 0;
 	while (str[i])
 		i++;
 	return (i);
 }
 
-char	*ft_strjoin(char *pre_line, char *buff)
+char	*ft_strchr(char *s, int c)
 {
 	size_t	i;
-	size_t	j;
-	char	*str;
 
-	if (!pre_line)
+	if (!s)
+		return (NULL);
+	if (c == 0)
 	{
-		pre_line = malloc(sizeof(char));
-		pre_line[0] = '\0';
+		i = ft_strlen((char *)s);
+		return (&s[i]);
 	}
-	if (!pre_line || !buff)
-		return (NULL);
-	str = malloc(sizeof(char) * ((ft_strlen(pre_line) + ft_strlen(buff)) + 1));
-	if (!str)
-		return (NULL);
-	i = -1;
-	j = 0;
-	if (pre_line)
-		while (pre_line[++i] != '\0')
-			str[i] = pre_line[i];
-	while (buff[j] != '\0')
-		str[i++] = buff[j++];
-	str[ft_strlen(pre_line) + ft_strlen(buff)] = '\0';
-	free(pre_line);
-	return (str);
-}
-
-char	*ft_line_break(char *pre_line, char c)
-{
-	int	i;
-
 	i = 0;
-	if (pre_line == NULL)
-		return (NULL);
-	while (pre_line[i] != '\0')
+	while (s[i])
 	{
-		if (pre_line[i] == c)
-			return (&pre_line[i]);
+		if (s[i] == (char) c)
+			return (&s[i]);
 		i++;
 	}
 	return (NULL);
 }
+
+char	*ft_join(char *dest, char *s1, char *s2)
+{
+	size_t i;
+	size_t j;
+
+	i = 0;
+	while (s1 && s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2 && s2[j])
+	{
+		dest [i + j] = s2[j];
+		j++;
+	}
+	dest [i + j] = '\0';
+	return (dest);
+}
+
+char	*ft_strjoin(char *start, char *buff)
+{
+	char	*ptr;
+
+	if(!start)
+	{
+		start = (char *)malloc(1 * sizeof(char));
+		start[0] = '\0';
+	}
+	if (!start || !buff)
+		return (NULL);
+	ptr = (char *)malloc(1 + ft_strlen(start) + ft_strlen(buff) * sizeof(char));
+	if (!ptr)
+		return (NULL);
+	ptr = ft_join(ptr, start, buff);
+	free(start);
+	return (ptr);
+}
+
